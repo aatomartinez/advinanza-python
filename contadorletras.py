c347a21e9aad7298c959e14c4d80832a09a321d5
Diego.marbou <aato.martinez@gmail.com>
2025-08-25 15:07:15 -0300

    Practica python cantidad de palabras

---
+++ contadorletras.py
@@ -0,0 +1,4 @@
+
+palabra_ingresada = input("Escribe una palabra asi cuento las letras: ")
+cantidad_letras = len(palabra_ingresada)
+print(f"La palabra, {palabra_ingresada} tiene {cantidad_letras} letras ")
